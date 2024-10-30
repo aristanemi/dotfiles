f0546c0dab411dad4e1fe9c57c466ad40da655ce
Abhishek Sunkum Rammurthy <sunkum@efr.de>
2024-10-30 12:05:22 +0100

    githooks from gcu-bkr

--- backup.sh
+++ backup.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+
+readonly SCRIPT_PATH=$(realpath "${BASH_SOURCE[0]}")
+readonly ABSOLUTE_DIR_PATH=$(dirname "$SCRIPT_PATH")
+
 source ./fileslist
 mkdir -p system
 for file in ${FILES[@]}; do
@@ -9,3 +13,8 @@ done
 # Create a directory for the ssh config file
 mkdir -p $PWD/system/.ssh
 cp ~/.ssh/config $PWD/system/.ssh/config
+
+# Copy git hooks from gcu-bkr
+mkdir -p git-hooks/gcu-bkr
+readonly GCU_BKR_HOOKS_DIR="$HOME/bind-vol/gcu-bkr/.git/hooks"
+cp -a $GCU_BKR_HOOKS_DIR $ABSOLUTE_DIR_PATH/git-hooks/gcu-bkr/
